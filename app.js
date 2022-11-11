e8da8f84bc5a73da5c7e0173fe2c55e95949e28e
snigdha-addepalli <85927559+snigdha-addepalli@users.noreply.github.com>
2022-11-11 20:04:36 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const app = express();
+const app = express(); 
 const { Todo } = require("./models");
 const cookieParser = require("cookie-parser");
 // const csrf = require("csurf");
