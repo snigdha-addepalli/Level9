e2356022aa9bd4e8370aad98552e578f39a0e829
snigdha-addepalli <85927559+snigdha-addepalli@users.noreply.github.com>
2022-11-13 13:34:50 +0530

    Update 20221001053837-create-todo.js

--- migrations/20221001053837-create-todo.js
+++ migrations/20221001053837-create-todo.js
@@ -3,7 +3,7 @@
 module.exports = {
   async up(queryInterface, Sequelize) {
     await queryInterface.createTable("Todos", {
-      id: {
+      id: { 
         allowNull: false,
         autoIncrement: true,
         primaryKey: true,
